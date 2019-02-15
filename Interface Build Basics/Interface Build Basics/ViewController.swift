a5733ef0520f38ffa7e2e2a05d1d7bafee99625c
Thenea <thenea@iMac4.local>
2019-02-15 10:57:58 -0500

    finished

---
+++ Interface Build Basics/Interface Build Basics/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Interface Build Basics
+//
+//  Created by Thenea on 2/14/19.
+//  Copyright © 2019 Thenea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func changeTitle(_ sender: UIButton) {
+        mainLabel.text = "This app rocks!"
+    }
+}
---
+++ Interface Build Basics/Interface Build BasicsTests/Interface_Build_BasicsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Interface_Build_BasicsTests.swift
+//  Interface Build BasicsTests
+//
+//  Created by Thenea on 2/14/19.
+//  Copyright © 2019 Thenea. All rights reserved.
+//
+
+import XCTest
+@testable import Interface_Build_Basics
+
+class Interface_Build_BasicsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
